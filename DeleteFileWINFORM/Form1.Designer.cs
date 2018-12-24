cbc2a349558ac3edc3753821740a93b70221aeda
Gunay <gunayaa@code.edu.az>
2018-12-24 13:06:00 +0400

    Unusued file and folder delete

---
+++ DeleteFileWINFORM/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace DeleteFileWINFORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblPathFile = new System.Windows.Forms.Label();
+            this.lblAccessFile = new System.Windows.Forms.Label();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderChsBtn = new System.Windows.Forms.Button();
+            this.dgvFiles = new System.Windows.Forms.DataGridView();
+            this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Day = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPathFile
+            // 
+            this.lblPathFile.AutoSize = true;
+            this.lblPathFile.Location = new System.Drawing.Point(23, 130);
+            this.lblPathFile.Name = "lblPathFile";
+            this.lblPathFile.Size = new System.Drawing.Size(0, 13);
+            this.lblPathFile.TabIndex = 2;
+            // 
+            // lblAccessFile
+            // 
+            this.lblAccessFile.AutoSize = true;
+            this.lblAccessFile.Location = new System.Drawing.Point(23, 204);
+            this.lblAccessFile.Name = "lblAccessFile";
+            this.lblAccessFile.Size = new System.Drawing.Size(0, 13);
+            this.lblAccessFile.TabIndex = 4;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(23, 61);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(0, 13);
+            this.lblFileName.TabIndex = 7;
+            // 
+            // folderChsBtn
+            // 
+            this.folderChsBtn.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.folderChsBtn.Location = new System.Drawing.Point(12, 27);
+            this.folderChsBtn.Name = "folderChsBtn";
+            this.folderChsBtn.Size = new System.Drawing.Size(116, 46);
+            this.folderChsBtn.TabIndex = 9;
+            this.folderChsBtn.Text = "Folder Choose";
+            this.folderChsBtn.UseVisualStyleBackColor = true;
+            this.folderChsBtn.Click += new System.EventHandler(this.folderChsBtn_Click_1);
+            // 
+            // dgvFiles
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.CornflowerBlue;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Open Sans", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.dgvFiles.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvFiles.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFiles.BackgroundColor = System.Drawing.Color.LightSteelBlue;
+            this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Path,
+            this.Day});
+            this.dgvFiles.Location = new System.Drawing.Point(151, 27);
+            this.dgvFiles.Name = "dgvFiles";
+            this.dgvFiles.Size = new System.Drawing.Size(701, 302);
+            this.dgvFiles.TabIndex = 10;
+            this.dgvFiles.RowHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvFiles_RowHeaderMouseDoubleClick);
+            // 
+            // Path
+            // 
+            this.Path.HeaderText = "Path";
+            this.Path.Name = "Path";
+            // 
+            // Day
+            // 
+            this.Day.HeaderText = "Day";
+            this.Day.Name = "Day";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(865, 341);
+            this.Controls.Add(this.dgvFiles);
+            this.Controls.Add(this.folderChsBtn);
+            this.Controls.Add(this.lblFileName);
+            this.Controls.Add(this.lblAccessFile);
+            this.Controls.Add(this.lblPathFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblPathFile;
+        private System.Windows.Forms.Label lblAccessFile;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Button folderChsBtn;
+        public System.Windows.Forms.FolderBrowserDialog folderDialog;
+        private System.Windows.Forms.DataGridView dgvFiles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Path;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Day;
+    }
+}
+
---
+++ DeleteFileWINFORM/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+namespace DeleteFileWINFORM
+{
+    public partial class Form1 : Form
+    {
+        public DirectoryInfo openedDir { get; set; }
+        public string PathDelete;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void fillDgv()
+        {
+            dgvFiles.Rows.Clear();
+            openedDir = new DirectoryInfo(folderDialog.SelectedPath);
+            string[] files = Directory.GetFiles(openedDir.FullName);
+            string[] directories = Directory.GetDirectories(openedDir.FullName);
+            DateTime now = DateTime.Now;
+            foreach (var item in files)
+            {
+                var file = Directory.GetLastAccessTime(item);
+                var yeni = now.Subtract(file);
+
+                if (yeni.Days > 91)
+                {
+                    dgvFiles.Rows.Add(item, yeni.Days);
+                }
+            }
+            foreach (var item in directories)
+            {
+                var dir = Directory.GetLastAccessTime(item);
+                var yeni = now.Subtract(dir);
+                if (yeni.Days > 91)
+                {
+                    dgvFiles.Rows.Add(item, yeni.Days);
+                }
+            }
+        }
+        private void folderChsBtn_Click_1(object sender, EventArgs e)
+        {
+            folderDialog.ShowDialog();
+            fillDgv();
+        }
+
+        private void dgvFiles_RowHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            PathDelete=dgvFiles.Rows[e.RowIndex].Cells[0].Value.ToString();
+
+            if (PathDelete.Contains("."))
+            {
+                DialogResult r = MessageBox.Show("File silmek isdediyinize eminsiniz?", "Delete", MessageBoxButtons.YesNo);
+                if (r == DialogResult.Yes)
+                {
+                    File.Delete(PathDelete);
+                }
+            }
+            else
+            {
+                DialogResult r = MessageBox.Show("Folder silmek isdediyinize eminsiniz?", "Delete", MessageBoxButtons.YesNo);
+                if (r == DialogResult.Yes)
+                {
+                    Directory.Delete(PathDelete, true);
+                }
+            }
+            fillDgv();
+        }
+    }
+}
